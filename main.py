0fa1074b5a58738d4c47b4c2ae909df31039cc81
Мойсей Елизавета <Moysey9g@mail.ru>
2022-01-26 09:27:26 +0300

    main2

--- main.py
+++ main.py
@@ -5,3 +5,23 @@ def x2(x):
 def x3_1(x):
     x = int(x * 3 + 1)
     collatz(x)
+    
+def collatz(x):
+    if x > 0:
+        if x == 1:
+            global L
+            L.append(x)
+            print(L)
+        else:
+            L.append(x)
+            if x % 2 == 0:
+                x2(x)
+            else:
+                x3_1(x)
+    else:
+        x = int(input('Введите число заново: '))
+        collatz(x)
+
+x = int(input('Начальное положительное значение = '))
+L = []
+collatz(x)
